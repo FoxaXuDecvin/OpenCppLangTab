e61f8827258bccdeb56aadbb402c31682d3e8ad1
Aways Exabyte <lzx13206010951@outlook.com>
2024-03-11 12:44:55 +0800

    1022 Release

--- CLang/SourceHeader/Settings/_sh_version.h
+++ CLang/SourceHeader/Settings/_sh_version.h
@@ -5,14 +5,14 @@
 
 using namespace std;
 
-string $version_title = "OpenCppLangTab _ (Preview)";
-string $version_msg = "Debug For Release";
+string $version_title = "OpenCppLangTab _ (Release)";
+string $version_msg = "Release";
 
 string $codename = "Lex";
 
-int $version_code = 1021;
+int $version_code = 1022;
 
-string $buildtime = "202403101652";
+string $buildtime = "202403111243";
 
 string $version_code_str = to_string($version_code);
 
