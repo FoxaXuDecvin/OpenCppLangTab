c2d785ce63789f9044d6dfe6c7bfeb591b516b1e
Aways Exabyte <lzx13206010951@outlook.com>
2024-06-22 19:45:36 +0800

    Fix OpenHttp Fail Download in chinese Environment

--- oclt_code/SourceHeader/Settings/_sh_version.h
+++ oclt_code/SourceHeader/Settings/_sh_version.h
@@ -5,13 +5,13 @@
 using namespace std;
 
 string $version_title = "OCLT _ 4 (Release)";
-string $version_msg = "Stable 15";
+string $version_msg = "Stable 16";
 
 string $codename = "Ocean";
 
-int $version_code = 4076;
+int $version_code = 4081;
 
-string $buildtime = "2024/06/20/15:18";
+string $buildtime = "2024/06/22/19:42";
 
 string $version_code_str = to_string($version_code);
 
--- oclt_code/SourceHeader/_sh_openhttp.h
+++ oclt_code/SourceHeader/_sh_openhttp.h
@@ -41,4 +41,23 @@ bool _urldown_api_nocache(string url, string savepath) {
 	//No Code
 }
 
+bool _urldown_api_vc_nocache(string url, string savepathA) {
+	string savepath = _get_random_s(1, 11111111);
+	url = url + "?voidcache=" + _get_random_s(1, 10000);
+	//Disabled Cache
+	_$BufferCache = URLDown(url, savepath);
+
+	//Decide
+	if (check_file_existenceA(savepath)) {
+		rename(savepath.c_str(), savepathA.c_str());
+		return true;
+	}
+	else {
+		_sh_throw_error("OpenHttp.Error :  Failed to Get URL :   " + url);
+		return false;
+	}
+
+	//No Code
+}
+
 //END
