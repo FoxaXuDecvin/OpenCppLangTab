40eb0b6883f896f6100ea569c81abb714c78d746
FoxaXu <lzx13206010951@outlook.com>
2025-10-22 22:33:36 +0800

    version editor

--- oclt_code/SourceHeader/Settings/_sh_version.h
+++ oclt_code/SourceHeader/Settings/_sh_version.h
@@ -7,13 +7,13 @@
 
 
 std::string $version_title = "OCLT _ 6 (Release)";
-std::string $version_msg = "Release 1";
+std::string $version_msg = "Release 2";
 
 std::string $codename = "Sunlight";
 
-int $version_code = 5002;
+int $version_code = 5006;
 
-std::string $buildtime = "2025/10/18/17:45";
+std::string $buildtime = "2025/10/22/22:33";
 
 std::string $version_code_str = std::to_string($version_code);
 
